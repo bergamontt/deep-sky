6746cbe610fddf89f55f0248ae0e837b3145f2b0
bergamontt <dariamumbo@gmail.com>
2025-10-04 12:13:54 +0300

    Added automatic download and processing of the large images

--- preprocessing/load_dzi_images.py
+++ preprocessing/load_dzi_images.py
@@ -1,11 +1,34 @@
 from pathlib import Path
+import requests
 import pyvips
+import os
+
+def download_images(urls, img_folder="img"):
+    Path(img_folder).mkdir(exist_ok=True)
+
+    downloaded_images = []
+    for url in urls:
+        filename = url.split('/')[-1]
+        file_path = os.path.join(img_folder, filename)
+
+        if os.path.exists(file_path):
+            continue
+
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(file_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+        downloaded_images.append(file_path)
+
+    return downloaded_images
 
 def valid_file(file):
-    valid_extensions = {'.jpg', '.jpeg', '.tiff', '.tif'}
+    valid_extensions = {".jpg", ".jpeg", ".tiff", ".tif"}
     return file.is_file() and file.suffix in valid_extensions
 
-def convert_to_dzi(input_folder, output_folder="storage"):
+def convert_to_dzi(input_folder="img", output_folder="storage"):
     input_path = Path(input_folder)
     output_path = Path(output_folder)
     output_path.mkdir(exist_ok=True)
@@ -20,10 +43,17 @@ def convert_to_dzi(input_folder, output_folder="storage"):
         image_output_dir.mkdir(exist_ok=True)
 
         dzi_output_path = str(image_output_dir / file_name)
-        image = pyvips.Image.new_from_file(str(file_path), access='sequential')
+        image = pyvips.Image.new_from_file(str(file_path), access="sequential")
         image.dzsave(dzi_output_path, tile_size=256, overlap=1)
 
-
 if __name__ == "__main__":
-    input_path_folder = "img"
-    convert_to_dzi(input_path_folder)
+
+    default_urls = [
+        "https://esahubble.org/media/archives/images/original/heic0707a.tif",
+        "https://esahubble.org/media/archives/images/original/heic0604a.tif",
+        "https://esahubble.org/media/archives/images/original/heic1302a.tif",
+        "https://esahubble.org/media/archives/images/original/heic0506a.tif"
+    ]
+
+    download_images(default_urls)
+    convert_to_dzi()
