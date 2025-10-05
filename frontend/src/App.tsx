d379f3bcbd824e9b4dd8626625ed815214b548d1
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-10-05 16:11:41 +0300

    Minor bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './pages/AppRoutes'
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import { Notifications } from '@mantine/notifications';
 
 function App() {
     return (
         <MantineProvider>
+            <Notifications />
             <BrowserRouter>
                 <AppRoutes />
             </BrowserRouter>
